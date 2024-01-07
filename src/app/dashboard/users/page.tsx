0da01b6e5dea8d29a1500eb47c1d51ef6d1bda29
javier torales <jtorales2016@gmail.com>
2024-01-07 06:27:05 -0300

    users page add delete modal

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,10 +1,10 @@
-import { SearchInput } from "@/app/UI/searchInput";
-import Image from "next/image";
+import { useState } from "react";
 import { filteredUsersByName, getAllUsers } from "@/app/lib/data";
 import { User } from "@/app/utilities/users";
-import { TableUsers } from "@/app/UI/table";
+import { TableUsers } from "@/app/UI/table/usersTable";
 import { Suspense } from "react";
-import { Pagination } from "@/app/UI/pagination";
+import { TableLayout } from "@/app/UI/tableLayout";
+import { Modal } from "@/app/UI/modal";
 export default async function Users({
   searchParams,
 }: {
@@ -32,37 +32,10 @@ export default async function Users({
   }
 
   return (
-    <>
-      <div className="w-full h-4/6 bg-firstWhite rounded-lg p-4">
-        <div className="h-14  w-full flex flex-row justify-between  items-center ">
-          <div className="w-2/12">
-            <SearchInput placeholder="Search for a user" />
-          </div>
-          <button className="bg-primary p-1 rounded-md text-firstWhite">
-            Add New
-          </button>
-        </div>
-        <div className="overflow-hidden rounded-lg ">
-          <Suspense fallback={"loading"} key={q + page}>
-            <TableUsers
-              users={users}
-              q={q}
-              redirect={() => {}}
-              files={[
-                { title: "Full name", wP: 3, id: 1 },
-                { title: "Email", wP: 3, id: 2 },
-                { title: "Created At", wP: 2, id: 3 },
-                { title: "Rol", wP: 1, id: 4 },
-                { title: "Status", wP: 1, id: 5 },
-                { title: "Action", wP: 2, id: 6 },
-              ]}
-            />
-          </Suspense>
-          <div className="mt-6">
-            <Pagination totalPages={total} currentPage={page} />
-          </div>
-        </div>
-      </div>
-    </>
+    <TableLayout>
+      <Suspense fallback={"loading"} key={q + page}>
+        <TableUsers users={users} totalPages={total} currentPage={page} />
+      </Suspense>
+    </TableLayout>
   );
 }
