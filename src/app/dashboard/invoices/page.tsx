15d13237be4ebd4c2274b8114921e8ad460f4d9c
javier torales <jtorales2016@gmail.com>
2024-01-07 12:12:02 -0300

    invoices page add

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -1,3 +1,46 @@
-export default function Map() {
-  return <>Map soy io</>;
+import { filteredInvoicessByName, getAllInvoices } from "@/app/lib/data";
+import { Invoices } from "@/app/utilities/invoices";
+import { Suspense } from "react";
+import { TableInvoices } from "@/app/UI/table/invoicesTable";
+import { TableLayout } from "@/app/UI/tableLayout";
+export default async function Invoices({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+    page?: string;
+  };
+}) {
+  let invoices = [] as Invoices[];
+  const q = searchParams?.q || "";
+  const page = Number(searchParams?.page) || 1;
+  let total = 1;
+  try {
+    if (q) {
+      let { invoicesArray, totalPages } = await filteredInvoicessByName(
+        q,
+        page
+      );
+      invoices = invoicesArray;
+      total = totalPages;
+    } else {
+      let { invoicesArray, totalPages } = await getAllInvoices(page);
+      invoices = invoicesArray;
+      total = totalPages;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  return (
+    <TableLayout>
+      <Suspense fallback={"loading"} key={q + page}>
+        <TableInvoices
+          invoices={invoices}
+          totalPages={total}
+          currentPage={page}
+        />
+      </Suspense>
+    </TableLayout>
+  );
 }
