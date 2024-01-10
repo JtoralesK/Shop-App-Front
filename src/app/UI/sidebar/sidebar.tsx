fa744a69d6db9cc5a296fe179886498cdcf99563
javier torales <jtorales2016@gmail.com>
2024-01-10 07:03:17 -0300

    menuLinks changed

--- src/app/UI/sidebar/sidebar.tsx
+++ src/app/UI/sidebar/sidebar.tsx
@@ -4,22 +4,39 @@ import { usePathname } from "next/navigation";
 import { linksNames } from "@/app/utilities/names/dashboardLinksNames";
 export function Sidebar() {
   const pathname = usePathname();
-  const active = pathname === "/dashboard";
+  const linksExceptLogout = linksNames.filter(
+    (linkName) => linkName.href !== "/login"
+  );
+  const logoutLink: any = linksNames.find(
+    (linkName) => linkName.href === "/login"
+  );
   return (
     <>
       <aside className="w-3/12 h-full bg-primary rounded-r-3xl ">
-        <div className="h-full flex flex-col place-items-center gap-2 pt-10 ">
-          {linksNames.map((linkName) => {
-            return (
-              <MenuLink
-                name={linkName.name}
-                icon={linkName.icon}
-                href={linkName.href}
-                key={linkName.key}
-                active={pathname === linkName.href}
-              />
-            );
-          })}
+        <div className="h-full flex flex-col justify-between  py-10 ">
+          <div className="flex flex-col place-items-center gap-2">
+            {linksExceptLogout.map((linkName) => {
+              return (
+                <MenuLink
+                  name={linkName.name}
+                  icon={linkName.icon}
+                  href={linkName.href}
+                  key={linkName.key}
+                  active={pathname === linkName.href}
+                />
+              );
+            })}
+          </div>
+          <div className="flex flex-col place-items-center">
+            <MenuLink
+              name={logoutLink.name}
+              icon={logoutLink.icon}
+              href={logoutLink.href}
+              key={logoutLink.key}
+              active={pathname === logoutLink.href}
+              top="mt-28"
+            />
+          </div>
         </div>
       </aside>
     </>
