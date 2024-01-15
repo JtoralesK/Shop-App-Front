8c8a24c73376c77f2407c5a37e56fa629286b611
javier torales <jtorales2016@gmail.com>
2024-01-15 11:39:30 -0300

    datespeaker component modified

--- src/app/UI/datespeaker.tsx
+++ src/app/UI/datespeaker.tsx
@@ -1,36 +1,20 @@
-"use client";
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { FaCalendarAlt } from "react-icons/fa";
-const CustomDatePicker = () => {
-  const [selectedDate, setSelectedDate] = useState(null);
+import { DefaultLabel } from "./labelText";
+type PickerProp = {
+  text: string;
+  value?: string;
+};
 
+export const DatePickerLabel = (p: PickerProp) => {
   return (
-    <div className=" w-full ">
-      <DatePicker
-        selected={selectedDate}
-        onChange={(date: any) => setSelectedDate(date)}
-        customInput={
-          <input
-            placeholder="DD/MM/YYYY"
-            value={selectedDate ? formatDate(selectedDate) : ""}
-            readOnly
-            className=" border w-full p-1 border-primary rounded-lg focus:border-primary focus:outline-none"
-          ></input>
-        }
-        dateFormat="dd/MM/yyyy"
+    <DefaultLabel text={p.text}>
+      <input
+        className="w-full py-1 border-2 bg-gray-100 rounded-md p-2"
+        type="date"
+        id="start"
+        min="2023-01-01"
+        max="2025-12-31"
+        value={p.value ? p.value : new Date().toISOString().slice(0, 10)}
       />
-    </div>
+    </DefaultLabel>
   );
 };
-
-// Helper function to format date as DD/MM/YYYY
-const formatDate = (date: any) => {
-  const year = date.getFullYear();
-  const month = `0${date.getMonth() + 1}`.slice(-2);
-  const day = `0${date.getDate()}`.slice(-2);
-  return `${day}/${month}/${year}`;
-};
-
-export default CustomDatePicker;
