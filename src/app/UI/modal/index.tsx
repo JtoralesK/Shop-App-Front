e82724644f201975512b45673b87fa0b97a97ea6
javier torales <jtorales2016@gmail.com>
2024-02-01 16:34:42 -0300

    modal add server action to delete user

--- src/app/UI/modal/index.tsx
+++ src/app/UI/modal/index.tsx
@@ -1,11 +1,16 @@
+import { deleteUserAction } from "@/app/lib/actions";
 import { IoMdClose } from "react-icons/io";
 type Props = {
   category: string;
   isOpen: boolean;
   itemId: number;
-  setOpen: (open: boolean) => void;
+  closeModal: (open: false) => void;
+  itemState: boolean;
 };
 export function Modal(p: Props) {
+  const change = () => {
+    p.closeModal(false);
+  };
   return (
     <>
       <div
@@ -17,33 +22,39 @@ export function Modal(p: Props) {
       >
         <dialog className="rounded-lg mt-40" open={p.isOpen}>
           <form
+            action={deleteUserAction}
             className="border p-3 p-3 bg-secondary rounded-lg "
-            method="dialog"
           >
             <div className="flex flex-row justify-end">
-              <button
-                type="button"
-                onClick={() => p.setOpen(false)}
-                className=""
-              >
+              <button type="button" onClick={() => p.closeModal(false)}>
                 <IoMdClose />
               </button>
             </div>
             <div className="flex justify-center pl-2 pr-2 pb-2">
               <div className="text-center ">
                 <p className="font-bold mt-2">
-                  You are about to delete a {p.category}
+                  You are about to {p.itemState ? "delete " : "active "}
+                  {p.category}
                 </p>
                 <p className="text-xs text-gray-700 font-medium mt-1">
-                  This will delete your {p.category} with id {p.itemId}
+                  This will {p.itemState ? "delete " : "active "} your{" "}
+                  {p.category} with id {p.itemId}
                 </p>
                 <p className="text-gray-700">Are you sure?</p>
               </div>
             </div>
             <div className="flex flex-row justify-end gap-3">
-              <button>Cancel</button>
-              <button className="bg-red-500 px-2 py-1 rounded-lg">
-                Delete
+              <button type="button" onClick={() => p.closeModal(false)}>
+                Cancel
+              </button>
+              <input type="hidden" name="id" value={p.itemId} />
+              <button
+                onClick={() => p.closeModal(false)}
+                className={` ${
+                  p.itemState ? "bg-red-400 " : "bg-green-400"
+                } p-2 h-10 rounded-lg `}
+              >
+                {p.itemState ? "Delete" : "Active"}
               </button>
             </div>
           </form>
