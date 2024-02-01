6c5c1b6929d1c4b9b4dc52a2465b20a14264d75d
javier torales <jtorales2016@gmail.com>
2024-02-01 16:19:05 -0300

    next auth config file add

---
+++ src/app/authconfig.ts
@@ -0,0 +1,22 @@
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  providers: [],
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }: any) {
+      const isLoggedIn = auth ? auth.user : false;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false;
+      }
+      if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      } else {
+        return false;
+      }
+      return true;
+    },
+  },
+};
