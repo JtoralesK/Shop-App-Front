672f44668ad80a07532141955bc182d5c2a744dd
javier torales <jtorales2016@gmail.com>
2024-03-06 07:53:48 -0300

    next authentication fix

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,6 +1,7 @@
+import { NextResponse } from "next/server";
+import { NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
-import { authConfig } from "./authconfig";
 
 const login = async (credentials: any) => {
   const body = JSON.stringify({
@@ -8,16 +9,43 @@ const login = async (credentials: any) => {
     password: credentials.password,
   });
   const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}api/auth/authenticate`;
-  const response = await fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: body,
-  });
-  const data = await response.json();
-  if (response.ok && data) {
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: body,
+    });
+    const data = await response.json();
+    if (data.errorResponse) {
+      throw new Error("Error al iniciar sesiónn");
+    }
     return data;
+  } catch (error) {
+    throw new Error("Error al iniciar sesión");
   }
-  return false;
+};
+
+export const authConfig = {
+  trustHost: true,
+  trustHostedDomain: true,
+  providers: [],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request }: any) {
+      const isLoggedIn = auth?.user;
+      const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", request.nextUrl));
+      }
+      return true;
+    },
+  },
 };
 
 export const { signIn, signOut, auth } = NextAuth({
@@ -26,14 +54,15 @@ export const { signIn, signOut, auth } = NextAuth({
     CredentialsProvider({
       async authorize(credentials) {
         try {
-          const loginResponse = await login(credentials);
-          return loginResponse;
-        } catch (e) {
+          const user = await login(credentials);
+          return user;
+        } catch (err) {
           return null;
         }
       },
     }),
   ],
+  // ADD ADDITIONAL INFORMATION TO SESSION
   callbacks: {
     async jwt({ token, user }: any) {
       if (user) {
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
-import { authConfig } from "./app/authconfig";
+import { authConfig } from "./app/auth";
 export default NextAuth(authConfig).auth;
-
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
